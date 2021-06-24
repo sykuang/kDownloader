e6b82b6527e3d7079ed93106c618894e64f19320
sykuang <sykuang.tw@gmail.com>
2021-06-24 10:33:36 +0800

    Update BooTransfer.ts

--- BooTransfer.ts
+++ BooTransfer.ts
@@ -1,33 +1,52 @@
-import * as path from 'path';
-import * as fs from 'fs';
-import * as  os from 'os';
 import * as  lib from './lib/helper';
-import operation from 'retry'
-const TakeMeBtn : string = "#shorturl-go > strong";
-const CloseBtn : string = "#btnCloseP";
-export async function getBooURL(browser : any, url : string) : Promise<any> {
+import * as argparse from 'argparse';
+import logger from 'loglevel';
+import prefix from 'loglevel-plugin-prefix';
+const TakeMeBtn: string = "#shorturl-go > strong";
+const CloseBtn: string = "#btnCloseP";
+export async function getBooURL(browser: any, url: string): Promise<any> {
   const page = await browser.newPage();
   await page.setDefaultNavigationTimeout(0);
-  await page.goto(url, 300000, "domcontentloaded");
+  logger.debug("goto: ", url);
+  await page.goto(url);
   for (var i = 0; i < 2; i++) {
-    var cnt : number = i + 2;
-    var CloseBtnWithIndex : string = CloseBtn + cnt + ""
-    await page.waitForSelector(CloseBtnWithIndex, 20000);
-    if (page.$(CloseBtnWithIndex)) {
-      try { await page.click(CloseBtnWithIndex) }
-      catch{ }
+    var cnt: number = i + 1;
+    var CloseBtnWithIndex: string = CloseBtn + cnt + ""
+    logger.debug("Waiting for ", CloseBtnWithIndex);
+    try {
+      await page.waitForSelector(CloseBtnWithIndex, 20000);
+      await page.click(CloseBtnWithIndex);
     }
+    catch {
+      logger.error("Click %s failed!", CloseBtnWithIndex);
+      if (i != 0) {
+        return page;
+      }
+    }
+    finally {logger.debug("Click %s done", CloseBtnWithIndex);}
     await lib.sleep(20000)
-    await page.waitForSelector(TakeMeBtn, 100000);
-    try { await page.click(TakeMeBtn) }
-    catch (error) { console.log("Cannot find the button") }
+    logger.debug("Waiting for ", TakeMeBtn);
+    try {
+      await page.waitForSelector(TakeMeBtn, 100000);
+      await page.click(TakeMeBtn);
+    }
+    catch (error) {logger.error("Click %s failed", TakeMeBtn)};
   }
   return page
 }
-let main = async function() : Promise<void> {
-  const retry = import('retry');
-  const browser = await lib.getBrowser()
-  const urls : Array<string> = process.argv.slice(2);
+let main = async function (): Promise<void> {
+  prefix.reg(logger);
+  logger.setLevel("warn");
+  const parser = new argparse.ArgumentParser({description: "Boo transfer"});
+  parser.add_argument('url', {help: 'Urls to transfer', type: "str", nargs: "+"});
+  parser.add_argument('--debug', {help: 'Enable debug mode', action: 'store_true'});
+  const args = parser.parse_args();
+  if (args.debug) {
+    logger.setLevel("trace");
+  }
+  prefix.apply(logger);
+  const urls: Array<string> = args.url;
+  const browser = await lib.getBrowser(!args.debug)
   for (const url of urls) {
     let page = await getBooURL(browser, url);
     let checkUrl = await page.evaluate(() => location.href)
--- lib/helper.ts
+++ lib/helper.ts
@@ -1,11 +1,11 @@
-import * as  puppeteer from 'puppeteer';
-export async function getBrowser(headlessMode=true) : Promise<any> {
+import puppeteer from 'puppeteer'
+export async function getBrowser(headlessMode = true): Promise<any> {
   const browser = await puppeteer.launch({
     headless: headlessMode,
     args: ['--no-sandbox'],
   });
   return browser
 }
-export function sleep(ms : number) : Promise<void> {
+export function sleep(ms: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms))
 }
